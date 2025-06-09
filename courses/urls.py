fd3aac709c7bc49975e6d63cf67ecda1d6e209e8
todd427 <todd427@gmail.com>
2025-06-09 11:01:07 -0700

    reset at page 996

--- courses/urls.py
+++ courses/urls.py
@@ -1,5 +1,9 @@
-from django.urls import path
+from django.urls import path, include
 from . import views
+
+
+app_name = 'courses'
+
 urlpatterns = [
     path(
         'mine/',
@@ -21,4 +25,5 @@ urlpatterns = [
         views.CourseDeleteView.as_view(),
         name='course_delete'
     ),
+  
 ]
--- courses/views.py
+++ courses/views.py
@@ -1,9 +1,7 @@
-from django.shortcuts import render
-from django.views.generic.list import ListView
-from django.views.generic.detail import DetailView
-from .models import Course, Module
 from django.urls import reverse_lazy
 from django.views.generic.edit import CreateView, UpdateView, DeleteView
+from django.views.generic import ListView
+from .models import Course
 from django.contrib.auth.mixins import LoginRequiredMixin, PermissionRequiredMixin
 
 class OwnerMixin(object):
@@ -11,71 +9,41 @@ class OwnerMixin(object):
         qs = super().get_queryset()
         return qs.filter(owner=self.request.user)
     
-class OwnerEditMixin:
+class OwnerEditMixin(object):
     def form_valid(self, form):
         form.instance.owner = self.request.user
         return super().form_valid(form)
     
-class OwnerCourseMixin(OwnerMixin, OwnerEditMixin, LoginRequiredMixin, PermissionRequiredMixin):
+class OwnerCourseMixin(OwnerMixin, LoginRequiredMixin, PermissionRequiredMixin):
     model = Course
     fields = ['subject', 'title', 'slug', 'overview']
-    template_name = 'courses/manage/course/form.html'
-    success_url = reverse_lazy('manage_course_list')
+    success_url = reverse_lazy('courses:manage_course_list')
 
-class OwnerCourseDeleteMixin(OwnerMixin, LoginRequiredMixin, PermissionRequiredMixin):
-    model = Course
-    success_url = reverse_lazy('manage_course_list')
-    template_name = 'courses/manage/course/delete.html'
-
-class OwnerCourseEditMixin(OwnerCourseMixin, CreateView):
+class OwnerCourseEditMixin(OwnerCourseMixin, OwnerEditMixin):
     template_name = 'courses/manage/course/form.html'
 
-class ManageCourseListView(ListView):
+class OwnerCourseEditMixin(OwnerCourseMixin,OwnerEditMixin):
+    template_name = 'courses/manage/course/form.html'
+
+class ManageCourseListView(OwnerCourseMixin, ListView):
     template_name = 'courses/manage/course/list.html'
     permission_required = 'courses.view_course'
 
-class CourseCreateView(OwnerCourseMixin, CreateView):
+class CourseCreateView(OwnerCourseEditMixin, CreateView):
     permission_required = 'courses.add_course'
 
 class CourseUpdateView(OwnerCourseEditMixin, UpdateView):
     permission_required = 'courses.change_course'
 
-class CourseDeleteView(OwnerCourseDeleteMixin, DeleteView):
-    permission_required = 'courses.delete_course'
-        
-
-class OwnerCourseModuleMixin(OwnerCourseMixin):
-    model = Module
-    fields = ['title', 'description']
-    template_name = 'courses/manage/module/formset.html'
-
-class OwnerCourseEditMixin(OwnerCourseMixin, UpdateView):
-    template_name = 'courses/manage/course/form.html' 
-    success_url = reverse_lazy('manage_course_list')
-
-class OwnerCourseDeleteMixin(OwnerCourseMixin, DeleteView):
+class CourseDeleteView(OwnerCourseMixin, DeleteView):
     template_name = 'courses/manage/course/delete.html'
-    success_url = reverse_lazy('manage_course_list')
+    permission_required = 'courses.delete_course'
 
-class CourseListView(ListView):
-    model = Course
-    context_object_name = 'courses'
-    template_name = 'courses/course/list.html'
-
-class CourseDetailView(DetailView):
-    model = Course
-    context_object_name = 'course'
-
-class ManageCourseListView(ListView):
-    model = Course
-    context_object_name = 'courses'
+class CourseListView(OwnerCourseMixin, ListView):
     template_name = 'courses/manage/course/list.html'
+    context_object_name = 'courses'
 
     def get_queryset(self):
         qs = super().get_queryset()
         return qs.filter(owner=self.request.user)
-
-class ManageCourseDetailView(DetailView):
-    model = Course
-    context_object_name = 'course'
-    template_name = 'courses/manage/course/detail.html'
+    
